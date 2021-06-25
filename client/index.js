23defb9e4cb5f5291ad38818d09845a04c625f1e
Sebastian Brear <ssbrear@ucdavis.edu>
2021-06-25 14:31:26 -0700

    I wanted to incorporate an API so I changed the purpose of the webpage to be a collection of random users.

--- client/index.js
+++ client/index.js
@@ -1,34 +1,36 @@
-const createProducts = () => {
-  const main = $("main");
-  const numProducts = 50;
-  for (let i = 0; i < numProducts; i++) {
-    // Creation of new elements
-    const newSection = $("<section>");
-    $(newSection).addClass("card");
+$.ajax({
+  url: "https://randomuser.me/api/?results=50",
+  dataType: "json",
+  success: function (data) {
+    const peopleArray = data.results;
+    const main = $("main");
+    peopleArray.forEach((person) => {
+      // Creation of new elements
+      const newSection = $("<section>");
+      $(newSection).addClass("card");
 
-    const newImage = $("<img>");
-    $(newImage).attr("src", "https://via.placeholder.com/300x200");
-    $(newImage).addClass("card-img-top");
+      const newImage = $("<img>");
+      $(newImage).attr("src", person.picture.large);
+      $(newImage).addClass("card-img-top");
 
-    const newBody = $("<div>");
-    $(newBody).addClass("card-body");
+      const newBody = $("<div>");
+      $(newBody).addClass("card-body");
 
-    const newTitle = $("<h2>").text("Title");
-    $(newTitle).addClass("card-title");
+      const newTitle = $("<h2>").text(person.name.first);
+      $(newTitle).addClass("card-title");
 
-    const newText = $("<p>").text(
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    );
-    $(newText).addClass("card-text");
+      const newText = $("<p>").text(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      );
+      $(newText).addClass("card-text");
 
-    // Attach new elements to existing elements
-    $(newBody).append(newTitle, newText);
-    $(newSection).append(newImage, newBody);
-    $(main).append(newSection);
-    console.log(newSection);
-  }
-};
-createProducts();
+      // Attach new elements to existing elements
+      $(newBody).append(newTitle, newText);
+      $(newSection).append(newImage, newBody);
+      $(main).append(newSection);
+    });
+  },
+});
 
 $("#burger, #modal").click(function (e) {
   if (checkInvalidModalChange(e.target)) return;
